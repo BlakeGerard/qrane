ecda05241a30adac469e83ac538edb95bc120f68
Blake Gerard <blake.t.gerard@gmail.com>
2022-07-31 10:44:43 -0500

    Cleanup and using clang-format now

--- include/qrane_scop.hpp
+++ include/qrane_scop.hpp
@@ -3,65 +3,71 @@
 
 #include <vector>
 
-#include "isl/union_set.h"
-#include "isl/union_map.h"
 #include "isl/ast_build.h"
-
-#include "qrane_deps.hpp"#ifndef QRANE_SCOP_H
+#include "isl/union_map.h"
+#include "isl/union_set.h"
+#include "qrane_deps.hpp" #ifndef QRANE_SCOP_H
 #define QRANE_SCOP_H
 
 #include <vector>
 
-#include "isl/union_set.h"
-#include "isl/union_map.h"
 #include "isl/ast_build.h"
-
+#include "isl/union_map.h"
+#include "isl/union_set.h"
 #include "qrane_deps.hpp"
 
 namespace qrane {
 
 struct full_scop_s {
-	isl_union_set* domain;
-	isl_union_map* read;
-	isl_union_map* write;
-	isl_union_map* call;
-	isl_union_map* schedule;
-	isl_union_map* dependencies;
-	isl_union_map* arguments;
+  isl_union_set* domain;
+  isl_union_map* read;
+  isl_union_map* write;
+  isl_union_map* call;
+  isl_union_map* schedule;
+  isl_union_map* dependencies;
+  isl_union_map* arguments;
 
-	~full_scop_s() {
-		isl_union_set_free(domain);
-		isl_union_map_free(read);
-		isl_union_map_free(write);
-		isl_union_map_free(call);
-		isl_union_map_free(schedule);
-		isl_union_map_free(dependencies);
-		isl_union_map_free(arguments);
-	}
+  ~full_scop_s() {
+    isl_union_set_free(domain);
+    isl_union_map_free(read);
+    isl_union_map_free(write);
+    isl_union_map_free(call);
+    isl_union_map_free(schedule);
+    isl_union_map_free(dependencies);
+    isl_union_map_free(arguments);
+  }
 };
 typedef full_scop_s full_scop;
 
 struct output_scop_s {
-	isl_union_set* domain;
-	isl_union_map* read;
-	isl_union_map* write;
-	isl_union_map* schedule;
-	isl_union_map* arguments;
+  isl_union_set* domain;
+  isl_union_map* read;
+  isl_union_map* write;
+  isl_union_map* schedule;
+  isl_union_map* arguments;
 
-	~output_scop_s() {
-		isl_union_set_free(domain);
-		isl_union_map_free(read);
-		isl_union_map_free(write);
-		isl_union_map_free(schedule);
-		isl_union_map_free(arguments);
-	}
+  output_scop_s(isl_ctx* ctx) {
+    domain = isl_union_set_empty_ctx(ctx);
+    read = isl_union_map_empty_ctx(ctx);
+    write = isl_union_map_empty_ctx(ctx);
+    schedule = isl_union_map_empty_ctx(ctx);
+    arguments = isl_union_map_empty_ctx(ctx);
+  }
+
+  ~output_scop_s() {
+    isl_union_set_free(domain);
+    isl_union_map_free(read);
+    isl_union_map_free(write);
+    isl_union_map_free(schedule);
+    isl_union_map_free(arguments);
+  }
 };
 typedef output_scop_s output_scop;
-}
+}  // namespace qrane
 
 struct s_qrane_ast_info_t {
-	isl_ast_expr_list* ast_exl;
-  	isl_ast_build* build;
+  isl_ast_expr_list* ast_exl;
+  isl_ast_build* build;
 };
 typedef struct s_qrane_ast_info_t t_qrane_ast_info;
 
@@ -70,48 +76,48 @@ typedef struct s_qrane_ast_info_t t_qrane_ast_info;
 namespace qrane {
 
 struct full_scop_s {
-	isl_union_set* domain;
-	isl_union_map* read;
-	isl_union_map* write;
-	isl_union_map* call;
-	isl_union_map* schedule;
-	isl_union_map* dependencies;
-	isl_union_map* arguments;
-	membership_map_t membership;
-	qubit_profile_map qubit_access_profile;
-	std::vector<Statement> final_domain_list;
+  isl_union_set* domain;
+  isl_union_map* read;
+  isl_union_map* write;
+  isl_union_map* call;
+  isl_union_map* schedule;
+  isl_union_map* dependencies;
+  isl_union_map* arguments;
+  membership_map_t membership;
+  qubit_profile_map qubit_access_profile;
+  std::vector<Statement> final_domain_list;
 
-	~full_scop_s() {
-		isl_union_set_free(domain);
-		isl_union_map_free(read);
-		isl_union_map_free(write);
-		isl_union_map_free(call);
-		isl_union_map_free(schedule);
-		isl_union_map_free(dependencies);
-		isl_union_map_free(arguments);
-	}
+  ~full_scop_s() {
+    isl_union_set_free(domain);
+    isl_union_map_free(read);
+    isl_union_map_free(write);
+    isl_union_map_free(call);
+    isl_union_map_free(schedule);
+    isl_union_map_free(dependencies);
+    isl_union_map_free(arguments);
+  }
 };
 
 struct output_scop_s {
-	isl_union_set* domain;
-	isl_union_map* read;
-	isl_union_map* write;
-	isl_union_map* schedule;
-	isl_union_map* arguments;
+  isl_union_set* domain;
+  isl_union_map* read;
+  isl_union_map* write;
+  isl_union_map* schedule;
+  isl_union_map* arguments;
 
-	~output_scop_s() {
-		isl_union_set_free(domain);
-		isl_union_map_free(read);
-		isl_union_map_free(write);
-		isl_union_map_free(schedule);
-		isl_union_map_free(arguments);
-	}
+  ~output_scop_s() {
+    isl_union_set_free(domain);
+    isl_union_map_free(read);
+    isl_union_map_free(write);
+    isl_union_map_free(schedule);
+    isl_union_map_free(arguments);
+  }
 };
-}
+}  // namespace qrane
 
 struct s_qrane_ast_info_t {
-	isl_ast_expr_list* ast_exl;
-  	isl_ast_build* build;
+  isl_ast_expr_list* ast_exl;
+  isl_ast_build* build;
 };
 typedef struct s_qrane_ast_info_t t_qrane_ast_info;
 
