c80d8ad08411f42d136f5e7ecd9875c9ec67f8f0
Blake Gerard <blake.t.gerard@gmail.com>
2024-08-04 17:33:48 -0500

    classic branch initial commit

---
+++ src/qrane_argument.hh
@@ -0,0 +1,41 @@
+/*
+Qrane
+Filename: qrane_argument.hh
+Creation date: June 30, 2020
+Copyright (C) 2020
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef QRANE_ARGUMENT
+#define QRANE_ARGUMENT
+
+#include <string>
+
+class qrane_argument {
+
+    protected:
+        std::string id;
+        unsigned long index;
+
+    public:
+        qrane_argument(std::string id);
+        qrane_argument(std::string id, unsigned long index);
+        qrane_argument();
+        ~qrane_argument();
+        std::string get_id();
+        unsigned long get_index();
+};
+
+#endif
---
+++ src/qrane_stmtlist.hh
@@ -0,0 +1,47 @@
+/*
+Qrane
+Filename: qrane_nodelist.hh
+Creation date: June 22, 2020
+Copyright (C) 2020
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef QRANE_STMTLIST
+#define QRANE_STMTLIST
+
+#include <vector>
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include "qrane_qop.hh"
+#include "qrane_statement.hh"
+
+class qrane_stmtlist {
+  private:
+    std::vector<qrane_statement*> stmts;
+
+  public:
+    ~qrane_stmtlist();
+	  void set_stmts(std::vector<qrane_statement*> given);
+    void add_statement(qrane_statement* statement);
+    std::vector<qrane_statement*> get_stmts();
+    std::vector<qrane_statement*> get_qops();
+    std::vector<qrane_statement*> get_1Q_gates();
+    std::vector<qrane_statement*> get_2Q_gates();
+    std::vector<qrane_statement*> get_qregs();
+    std::vector<unsigned long> get_qops_first_argument_indices();
+};
+
+#endif
